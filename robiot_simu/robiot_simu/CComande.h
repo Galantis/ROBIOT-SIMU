b84251ec98e66c94e836caf360714719a9740b7a
Lucas_LG <Lucas@DESKTOP-OGB8RNM>
2020-06-15 11:01:05 +0200

    Ajout de la classe CComande

---
+++ robiot_simu/robiot_simu/CComande.h
@@ -0,0 +1,15 @@
+#pragma once
+/**************************************************************
+* CLASSE : <CCommande> 
+* PRESENTATION : <description de la classe et de sa fonction> 
+*
+* METHODES PUBLIQUES : * <Methode1> : <description rapide de la methode 1> 
+* <Methode2> : <description rapide de la methode 2> 
+* <Methode3> : <description rapide de la methode 3> 
+* <Methode4> : <description rapide de la methode 4> 
+*
+* OBSERVATIONS : **************************************************************/
+class CComande
+{
+};
+
