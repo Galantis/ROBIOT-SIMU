dcab3fd0c1d3a04ebe40d60e1ad538f804d7296d
Lucas_LG <Lucas@DESKTOP-OGB8RNM>
2020-06-15 11:09:12 +0200

    Mise à jour des entête "cartouche" des classes

--- robiot_simu/robiot_simu/CBatterie.cpp
+++ robiot_simu/robiot_simu/CBatterie.cpp
@@ -1,2 +1,12 @@
 #include "pch.h"
 #include "CBatterie.h"
+/**************************************************************
+* CLASSE : <CBatterie>
+* PRESENTATION : <description de la classe et de sa fonction>
+*
+* METHODES PUBLIQUES : * <Methode1> : <description rapide de la methode 1>
+* <Methode2> : <description rapide de la methode 2>
+* <Methode3> : <description rapide de la methode 3>
+* <Methode4> : <description rapide de la methode 4>
+*
+* OBSERVATIONS : **************************************************************/
--- robiot_simu/robiot_simu/pch.cpp
+++ robiot_simu/robiot_simu/pch.cpp
@@ -4,3 +4,13 @@
 //
 
 #include "pch.h"
+/**************************************************************
+* CLASSE : <pch>
+* PRESENTATION : <description de la classe et de sa fonction>
+*
+* METHODES PUBLIQUES : * <Methode1> : <description rapide de la methode 1>
+* <Methode2> : <description rapide de la methode 2>
+* <Methode3> : <description rapide de la methode 3>
+* <Methode4> : <description rapide de la methode 4>
+*
+* OBSERVATIONS : **************************************************************/
